71a2996b696b2ec5addbea691e3217752bf44189
badahertz52 <badahertz52@gmail.com>
2025-01-17 19:11:32 +0900

    ci : emotion,stylelint 설치 및 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,57 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "typeAlias",
+          format: ["PascalCase"],
+        },
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+        },
+      ],
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          groups: [
+            ["builtin", "external"],
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern: "next",
+              group: "builtin",
+            },
+            {
+              pattern: "react",
+              group: "builtin",
+            },
+            {
+              pattern: "@MyDesignSystem/**",
+              group: "internal",
+            },
+            {
+              pattern: "src/**",
+              group: "internal",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["src/**", "@MyDesignSystem/**"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
