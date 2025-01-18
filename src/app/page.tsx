bb342f01ccfa99bb4c7ad2d3623a169d853f8fd1
badahertz52 <badahertz52@gmail.com>
2025-01-18 22:17:37 +0900

    ci : tailwind 삭제 , scss 설치
    - 삭제 이유 : 반응형, 동적 스타일링에 재한 있음

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-500 text-primary">
-      <h1 className="text-4xl font-bold">Tailwind CSS is working!</h1>
+    <div>
+      <h1>Tailwind CSS is working!</h1>
     </div>
   );
 }
