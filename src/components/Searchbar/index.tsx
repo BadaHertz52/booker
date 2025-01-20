9d02969d31745b08ff9f187682707efbac1d5304
badahertz52 <badahertz52@gmail.com>
2025-01-20 23:44:20 +0900

    feat : 드롭다운 메뉴 구현 및 디바운스 유틸 추가

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -6,6 +6,7 @@ import React, { useState } from 'react';
 
 import FullTriangle from '@/images/fullTriangle.svg';
 import SearchIcon from '@/images/searchIcon.svg';
+import { debounce } from '@/utils/debounce';
 
 import styles from './index.module.scss';
 type SearchCategory = 'title' | 'author';
@@ -15,36 +16,70 @@ const CATEGORY_NAME: Record<SearchCategory, string> = {
   author: '저자',
 } as const;
 
+const ELEMENT_ID = {
+  searchCategoryList: 'search-category-list',
+  searchInput: 'searchInput',
+  dropdownMenuButton: 'dropdownMenuButton',
+} as const;
+
+const DELAY_TIME = 1 * 100;
 interface CategoryDropdownMenuProps {
   category: SearchCategory;
   changeCategory: () => void;
 }
 const CategoryDropdownMenu = ({ category, changeCategory }: CategoryDropdownMenuProps) => {
-  return <></>;
+  return (
+    <ul id={ELEMENT_ID.searchCategoryList} className={styles.dropdownMenu}>
+      {Object.entries(CATEGORY_NAME).map(([key, value]) => (
+        <li key={key} role="option" aria-selected={category === value} tabIndex={category === value ? 0 : -1}>
+          {value}
+        </li>
+      ))}
+    </ul>
+  );
 };
 const Searchbar = () => {
   const [category, setCategory] = useState<SearchCategory>('title');
-  const [isOpenMenu, setIsOpenMenu] = useState(true);
+  const [isOpenDropdownMenu, setIsOpenDropdownMenu] = useState(true);
+
+  const handleClickDropdownOpenButton = debounce(() => {
+    setIsOpenDropdownMenu((prev) => !prev);
+  }, DELAY_TIME);
+
   const changeCategory = () => {};
   return (
-    <div className={styles.searchbar}>
-      <button className={styles.categoryContainer}>
-        <span>{CATEGORY_NAME[category]}</span>
+    <form className={styles.searchbar}>
+      <button
+        id={ELEMENT_ID.dropdownMenuButton}
+        type="button"
+        className={styles.categoryContainer}
+        aria-haspopup="listbox"
+        aria-expanded="false"
+        aria-controls={ELEMENT_ID.searchCategoryList}
+        onClick={handleClickDropdownOpenButton}
+      >
+        <span title="검색유형">{CATEGORY_NAME[category]}</span>
         <Image
-          className={classNames({ [styles.reverseTriangle]: isOpenMenu })}
+          className={classNames(styles.triangle, { [styles.reverse]: isOpenDropdownMenu })}
           src={FullTriangle}
           alt=""
           width={10}
           height={8}
+          aria-hidden="true"
         />
-        <span className={styles.bar}>|</span>
+        <span aria-hidden="true" className={styles.bar}>
+          |
+        </span>
       </button>
-      {isOpenMenu && <CategoryDropdownMenu category={category} changeCategory={changeCategory} />}
-      <input placeholder="검색어를 입력해주세요" />
+      {isOpenDropdownMenu && <CategoryDropdownMenu category={category} changeCategory={changeCategory} />}
+      <label className="sr-only" htmlFor={ELEMENT_ID.searchInput}>
+        검색어
+      </label>
+      <input id={ELEMENT_ID.searchInput} type="search" name="searchValue" placeholder="검색어를 입력해주세요" />
       <button className={styles.searchButton}>
         <Image src={SearchIcon} alt="" width={17} height={18} />
       </button>
-    </div>
+    </form>
   );
 };
 
