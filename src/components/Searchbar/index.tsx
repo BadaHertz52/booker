fc1d040626cddea8bfccb036c99abb9d24809e35
badahertz52 <badahertz52@gmail.com>
2025-01-20 23:54:41 +0900

    refactor: useElementId 훅 생성
    - 추후 검색바 재사용을 위해, element id 중복을 피하기 위해 컴포넌트별로 id를 다르게 만드는 훅 생성

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -2,12 +2,13 @@
 
 import classNames from 'classnames';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import FullTriangle from '@/images/fullTriangle.svg';
 import SearchIcon from '@/images/searchIcon.svg';
 import { debounce } from '@/utils/debounce';
 
+import useElementId from './hooks/useElementId';
 import styles from './index.module.scss';
 type SearchCategory = 'title' | 'author';
 
@@ -16,20 +17,15 @@ const CATEGORY_NAME: Record<SearchCategory, string> = {
   author: '저자',
 } as const;
 
-const ELEMENT_ID = {
-  searchCategoryList: 'search-category-list',
-  searchInput: 'searchInput',
-  dropdownMenuButton: 'dropdownMenuButton',
-} as const;
-
 const DELAY_TIME = 1 * 100;
 interface CategoryDropdownMenuProps {
+  id: string;
   category: SearchCategory;
   changeCategory: () => void;
 }
-const CategoryDropdownMenu = ({ category, changeCategory }: CategoryDropdownMenuProps) => {
+const CategoryDropdownMenu = ({ id, category, changeCategory }: CategoryDropdownMenuProps) => {
   return (
-    <ul id={ELEMENT_ID.searchCategoryList} className={styles.dropdownMenu}>
+    <ul id={id} className={styles.dropdownMenu}>
       {Object.entries(CATEGORY_NAME).map(([key, value]) => (
         <li key={key} role="option" aria-selected={category === value} tabIndex={category === value ? 0 : -1}>
           {value}
@@ -40,22 +36,39 @@ const CategoryDropdownMenu = ({ category, changeCategory }: CategoryDropdownMenu
 };
 const Searchbar = () => {
   const [category, setCategory] = useState<SearchCategory>('title');
-  const [isOpenDropdownMenu, setIsOpenDropdownMenu] = useState(true);
+  const [isOpenDropdownMenu, setIsOpenDropdownMenu] = useState(false);
+  const elementId = useElementId();
 
   const handleClickDropdownOpenButton = debounce(() => {
     setIsOpenDropdownMenu((prev) => !prev);
   }, DELAY_TIME);
 
+  const handleDropdownMenu = (e: MouseEvent) => {
+    if (!(e.target instanceof HTMLElement)) return;
+
+    if (e.target.closest(`#${elementId.searchCategoryList}`) || e.target.closest(`#${elementId.dropdownMenuButton}`))
+      return;
+    setIsOpenDropdownMenu(false);
+  };
+
   const changeCategory = () => {};
+
+  useEffect(() => {
+    document.addEventListener('click', handleDropdownMenu);
+    return () => {
+      document.removeEventListener('click', handleDropdownMenu);
+    };
+  }, []);
+
   return (
     <form className={styles.searchbar}>
       <button
-        id={ELEMENT_ID.dropdownMenuButton}
+        id={elementId.dropdownMenuButton}
         type="button"
         className={styles.categoryContainer}
         aria-haspopup="listbox"
         aria-expanded="false"
-        aria-controls={ELEMENT_ID.searchCategoryList}
+        aria-controls={elementId.searchCategoryList}
         onClick={handleClickDropdownOpenButton}
       >
         <span title="검색유형">{CATEGORY_NAME[category]}</span>
@@ -71,11 +84,13 @@ const Searchbar = () => {
           |
         </span>
       </button>
-      {isOpenDropdownMenu && <CategoryDropdownMenu category={category} changeCategory={changeCategory} />}
-      <label className="sr-only" htmlFor={ELEMENT_ID.searchInput}>
+      {isOpenDropdownMenu && (
+        <CategoryDropdownMenu id={elementId.searchCategoryList} category={category} changeCategory={changeCategory} />
+      )}
+      <label className="sr-only" htmlFor={elementId.searchInput}>
         검색어
       </label>
-      <input id={ELEMENT_ID.searchInput} type="search" name="searchValue" placeholder="검색어를 입력해주세요" />
+      <input id={elementId.searchInput} type="search" name="searchValue" placeholder="검색어를 입력해주세요" />
       <button className={styles.searchButton}>
         <Image src={SearchIcon} alt="" width={17} height={18} />
       </button>
