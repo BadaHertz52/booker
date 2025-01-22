f7745209b0e8d77695d34e49640db26fc35e6d20
badahertz52 <badahertz52@gmail.com>
2025-01-22 21:29:13 +0900

    ci : 스토리북 설치 및 설정

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from '@storybook/react';
+
+import '@/styles/variables.scss';
+import '@/styles/globals.scss';
+import '@/styles/reset.scss';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
