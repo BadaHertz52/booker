15b37e92196688fdf5cabfa00ce153b1f1ee978a
badahertz52 <badahertz52@gmail.com>
2025-01-24 17:03:40 +0900

    style : 스토리북 - 프리텐다드 폰트 적용, 기본 레이아웃 스타일 적용

---
+++ .storybook/preview.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import type { Preview } from '@storybook/react';
+
+import '@/styles/variables.scss';
+import '@/styles/globals.scss';
+import '@/styles/reset.scss';
+
+const GlobalStyles = `
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff2');
+    font-display: swap;
+  }
+
+  body {
+    font-family: 'Pretendard',  sans-serif;
+  }
+  .storyWrapper{
+   margin: 2.4rem 
+   
+  }
+`;
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div>
+        <style>{GlobalStyles}</style> <div className="storyWrapper">{Story()}</div>
+      </div>
+    ),
+  ],
+};
+
+export default preview;
