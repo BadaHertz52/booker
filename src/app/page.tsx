1a37ad2780cace876b395a9f7b1e90187881b2b0
badahertz52 <badahertz52@gmail.com>
2025-01-27 19:34:47 +0900

    style : 사서 추천 도서 무한 스크롤 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
 import InfinityCarousel from '@/components/common/InfinityCarousel';
 
+import styles from './page.module.scss';
+
 export default function Home() {
   return (
     <div>
-      <h1>✨🍀📚</h1>
-      <h2>사서 추천 도서</h2>
-      <InfinityCarousel
-        title="사서 추천 도서"
-        cardInfoForScreenReader={[
-          { title: '책1' },
-          { title: '책2' },
-          { title: '책3' },
-          { title: '책4' },
-          { title: '책5' },
-        ]}
-      >
-        {[{ id: '책1' }, { id: '책2' }, { id: '책3' }, { id: '책4' }, { id: '책5' }].map((book) => (
-          <div key={book.id} style={{ width: '300px', height: '100px', backgroundColor: 'skyblue' }}>
-            {book.id}
-          </div>
-        ))}
-      </InfinityCarousel>
+      <section className={styles.section}>
+        <h2>사서 추천 도서</h2>
+        <div className={styles.recommendBooksCarouselWrapper}>
+          <InfinityCarousel
+            title="사서 추천 도서"
+            cardInfoForScreenReader={[
+              { title: '책1' },
+              { title: '책2' },
+              { title: '책3' },
+              { title: '책4' },
+              { title: '책5' },
+            ]}
+          >
+            {[{ id: '책1' }, { id: '책2' }, { id: '책3' }, { id: '책4' }, { id: '책5' }].map((book) => (
+              <div key={book.id} className={styles.fullWidthCard}>
+                {book.id}
+              </div>
+            ))}
+          </InfinityCarousel>
+        </div>
+      </section>
     </div>
   );
 }
