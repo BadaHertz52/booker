2c67b030f7549aef288420411e872a42a59057ef
badahertz52 <badahertz52@gmail.com>
2025-01-27 22:56:14 +0900

    feat: BooksInfinityCarousel 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import InfinityCarousel from '@/components/common/InfinityCarousel';
+import { BooksInfinityCarousel } from '@/components';
 import { RECOMMENDED_BOOKS_MOCK_DATA } from '@/mocks/mockData';
 
 import styles from './page.module.scss';
 
 export default function Home() {
   const recommendedBooks = RECOMMENDED_BOOKS_MOCK_DATA;
-  const recommendedBookCardsInfo = recommendedBooks.map((book) => ({ title: book.title }));
 
   return (
     <div>
       <section className={styles.section}>
         <h2>사서 추천 도서</h2>
         <div className={styles.recommendBooksCarouselWrapper}>
-          <InfinityCarousel title="사서 추천 도서" cardInfoForScreenReader={recommendedBookCardsInfo}>
-            {recommendedBooks.map((book) => (
-              <div key={book.isbn} className={styles.fullWidthCard}>
-                {book.title}
-              </div>
-            ))}
-          </InfinityCarousel>
+          <BooksInfinityCarousel booksSimpleInfo={recommendedBooks} title="사서 추천 도서" />
         </div>
       </section>
     </div>
