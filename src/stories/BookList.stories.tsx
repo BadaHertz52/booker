3ef113cf3b8be6565490c1825acb2b786ccada75
badahertz52 <badahertz52@gmail.com>
2025-01-28 19:07:06 +0900

    refactor : 목 데이터에 대출 급상승 도서 추가

--- src/stories/BookList.stories.tsx
+++ src/stories/BookList.stories.tsx
@@ -1,13 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { BookList, BookListSkeleton } from '@/components';
-import { RECOMMENDED_BOOKS_MOCK_DATA } from '@/mocks/mockData';
-import { BookItemData } from '@/types';
-
-const HOT_BOOKS: BookItemData[] = RECOMMENDED_BOOKS_MOCK_DATA.map((book) => ({
-  ...book,
-  publicationYear: 2025,
-}));
+import { HOT_BOOKS_MOCK_DATA } from '@/mocks/mockData';
 
 const TITLE = '대출 급상승 도서';
 
@@ -26,7 +20,7 @@ type Story = StoryObj<typeof BookList>;
 export const Default: Story = {
   args: {
     listTitle: TITLE,
-    bookItemsData: HOT_BOOKS,
+    bookItemsData: HOT_BOOKS_MOCK_DATA,
   },
 };
 
