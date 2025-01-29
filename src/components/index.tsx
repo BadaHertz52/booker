64a6ea31c0d39a140d2987d7bc67af41fa8a6278
badahertz52 <badahertz52@gmail.com>
2025-01-29 15:25:20 +0900

    refactor : 컴포넌트 폴더 구조 재배치 (#28)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as Footer } from './Footer';
-export { default as Topbar } from './Topbar/index';
-export * from './common';
+export { default as Topbar } from './Topbar';
+export * from './overlay';
 export * from './carousel';
 export * from './book';
+export * from './search';
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,2 +1,1 @@
-export { default as Toast } from './Toast';
 export { default as Searchbar } from './Searchbar';
