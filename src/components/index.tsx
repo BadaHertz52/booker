db7b0e4044822fe032861cde745a5e9d4d8af695
badahertz52 <badahertz52@gmail.com>
2025-01-29 15:02:47 +0900

    refactor : Searchbar 컴포넌트 search 폴더로 이동

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as Footer } from './Footer';
-export { default as Topbar } from './Topbar/index';
+export { default as Topbar } from './Topbar';
 export * from './common';
 export * from './carousel';
 export * from './book';
+export * from './search';
