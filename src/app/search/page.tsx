b6312cb9b792ac7190b15fc51a7cb74ed9212aea
badahertz52 <badahertz52@gmail.com>
2025-01-29 19:48:52 +0900

    feat : 검색 페이지 - 검색 결과에 따른 도서 목록, 찾는 정보 없음 표시 분기 처리

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,6 @@
-import { BookList } from '@/components';
+import { BookList, NotFoundSearchResult } from '@/components';
 import { FRONTEND_BOOKS_MOCK_DATA } from '@/mocks/mockData';
+import { BookItemData } from '@/types';
 
 import styles from './page.module.scss';
 
@@ -16,15 +17,26 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
   const params = await searchParams;
   const { keyword, category } = params;
 
+  const searchResult: BookItemData[] | null = null;
+
   const truncatedKeyword =
     keyword.length > MAX_SEARCH_KEYWORD_LENGTH ? keyword.slice(0, MAX_SEARCH_KEYWORD_LENGTH) + '...' : keyword;
   const searchResultGuideMessage = `'${truncatedKeyword}'에 대한 검색 결과에요.`;
   const searchResultListTitle = `${keyword} 검색 결과`;
+  const searchTarget = category === 'author' ? '저자' : '도서';
 
   return (
-    <div>
-      <BookList listTitle={searchResultListTitle} bookItemsData={FRONTEND_BOOKS_MOCK_DATA} />
+    <div className={styles.container}>
       <h1 className={styles.searchResultGuideMessage}>{searchResultGuideMessage}</h1>
+      {searchResult ? (
+        <BookList listTitle={searchResultListTitle} bookItemsData={FRONTEND_BOOKS_MOCK_DATA} />
+      ) : (
+        <div className={styles.notFoundResultWrapper}>
+          <NotFoundSearchResult>
+            <p>찾으시는 {searchTarget} 정보가 없어요.</p>
+          </NotFoundSearchResult>
+        </div>
+      )}
     </div>
   );
 };
