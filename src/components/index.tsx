619935ecfcde893b0d50176dc6a247858b8c507e
badahertz52 <badahertz52@gmail.com>
2025-01-29 15:04:10 +0900

    refactor : common 폴더명 overlay로 변경

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 export { default as Footer } from './Footer';
 export { default as Topbar } from './Topbar';
-export * from './common';
+export * from './overlay';
 export * from './carousel';
 export * from './book';
 export * from './search';
