c55287f0bc0ad5fbe05e690b0e8272bba5a39e44
badahertz52 <badahertz52@gmail.com>
2025-01-29 20:04:38 +0900

    feat : 검색 응답 상태에 따른 UI 분기 처리
    - 검색 API를 연결 작업을 위한 사전 작업 진행

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,5 @@
-import { BookList, NotFoundSearchResult } from '@/components';
+import { BookList, BookListSkeleton, NotFoundSearchResult } from '@/components';
 import { FRONTEND_BOOKS_MOCK_DATA } from '@/mocks/mockData';
-import { BookItemData } from '@/types';
 
 import styles from './page.module.scss';
 
@@ -12,25 +11,29 @@ interface SearchPageProps {
 }
 
 const MAX_SEARCH_KEYWORD_LENGTH = 16;
+const SKELETON_LIST_LENGTH = 5;
 
 const SearchPage = async ({ searchParams }: SearchPageProps) => {
   const params = await searchParams;
   const { keyword, category } = params;
-
-  const searchResult: BookItemData[] | null = null;
-
+  // TODO : 검색 api 응답 상태에 따라 추후 세부 작업 진행
+  const result: { state: 'pending' | 'success' | 'fail' } = { state: 'success' };
   const truncatedKeyword =
     keyword.length > MAX_SEARCH_KEYWORD_LENGTH ? keyword.slice(0, MAX_SEARCH_KEYWORD_LENGTH) + '...' : keyword;
-  const searchResultGuideMessage = `'${truncatedKeyword}'에 대한 검색 결과에요.`;
+  const searchResultGuideMessage = `'${truncatedKeyword}'에 대한 검색 ${result.state === 'pending' ? '중 이에요' : '결과에요'}`;
   const searchResultListTitle = `${keyword} 검색 결과`;
   const searchTarget = category === 'author' ? '저자' : '도서';
 
   return (
     <div className={styles.container}>
       <h1 className={styles.searchResultGuideMessage}>{searchResultGuideMessage}</h1>
-      {searchResult ? (
+      {result.state === 'pending' && (
+        <BookListSkeleton listTitle={searchResultListTitle} listLength={SKELETON_LIST_LENGTH} />
+      )}
+      {result.state === 'success' && (
         <BookList listTitle={searchResultListTitle} bookItemsData={FRONTEND_BOOKS_MOCK_DATA} />
-      ) : (
+      )}
+      {result.state === 'fail' && (
         <div className={styles.notFoundResultWrapper}>
           <NotFoundSearchResult>
             <p>찾으시는 {searchTarget} 정보가 없어요.</p>
