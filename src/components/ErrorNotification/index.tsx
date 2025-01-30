1b863b21c3f7574b8604926b9b3a99da03558f55
badahertz52 <badahertz52@gmail.com>
2025-01-30 21:05:43 +0900

    feat : ErrorNotification 컴포넌트 구현
    - 에러 메세지, reset을 props로 받음
    - 에러 아이콘, 에러 메세지, 에러 대응 방법(홈 이동, 다시 시도하기) 제공
    - gray200으로

---
+++ src/components/ErrorNotification/index.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import { gray200BlurDataURL } from '@/constants';
+import ErrorIcon from '@/images/error.svg';
+
+import styles from './index.module.scss';
+
+interface ErrorNotificationProps {
+  children: React.ReactNode;
+  reset: () => void;
+}
+const ErrorNotification = ({ children, reset }: ErrorNotificationProps) => {
+  const router = useRouter();
+
+  const moveToHome = () => {
+    router.push('/');
+  };
+  return (
+    <div className={styles.errorNotification}>
+      <Image src={ErrorIcon} alt="" width={140} height={140} placeholder="blur" blurDataURL={gray200BlurDataURL} />
+      {children}
+      <button className={styles.resetButton} onClick={reset}>
+        다시 시도하기
+      </button>
+      <button className={styles.homeButton} onClick={moveToHome}>
+        홈으로 돌아가기
+      </button>
+    </div>
+  );
+};
+
+export default ErrorNotification;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as Footer } from './Footer';
 export { default as Topbar } from './Topbar';
+export { default as ErrorNotification } from './ErrorNotification';
 export * from './overlay';
 export * from './carousel';
 export * from './book';
