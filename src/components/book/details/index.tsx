68467f6389dc15550ad2b15e0c8d412ae7bd1c72
badahertz52 <badahertz52@gmail.com>
2025-02-01 20:08:54 +0900

    feat : BookDetail 컴포넌트 구현

--- src/components/book/details/index.tsx
+++ src/components/book/details/index.tsx
@@ -10,40 +10,71 @@ interface BookDetailProps {
 }
 
 const BookDetails = ({ bookDetailData }: BookDetailProps) => {
-  const { contents, author, publisher, publicationYear, category, loans } = bookDetailData;
+  const { title, contents, author, publisher, publicationYear, publicationDate, category, loans, isbn } =
+    bookDetailData;
   const bookSummary = [
     `${author} 저자(글)`,
-    `${publisher} - ${publicationYear}년`,
-    `${category}`,
-    `대출 순위 - ${loans.rank} (${loans.count}건)`,
+    `${publisher}﹒${publicationYear}년`,
+    `대출 ${loans.rank}위 (${loans.count}건)`,
   ];
+
+  const formateDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return `${year}년 ${month}월 ${day}일`;
+  };
+
   return (
-    <div>
-      <section>
-        <div
-          className={styles.bookSummaryContainer}
-          style={{ backgroundImage: `url(${bookDetailData.coverImageUrl})` }}
-        >
-          <div className={styles.coverImageWrapper}>
-            <Image
-              src={bookDetailData.coverImageUrl}
-              alt={bookDetailData.title}
-              fill
-              placeholder="blur"
-              blurDataURL={gray200BlurDataURL}
-            />
-            <h1>{bookDetailData.title}</h1>
-            <ul>
-              {bookSummary.map((summary) => (
-                <li key={summary}>{summary}</li>
-              ))}
-            </ul>
-          </div>
+    <section>
+      <div className={styles.coverImageContainer} style={{ backgroundImage: `url(${bookDetailData.coverImageUrl})` }}>
+        <div className={styles.coverImageWrapper}>
+          <Image
+            src={bookDetailData.coverImageUrl}
+            alt={bookDetailData.title}
+            fill
+            placeholder="blur"
+            blurDataURL={gray200BlurDataURL}
+          />
         </div>
-        <div>{contents}</div>
-      </section>
-      <section>도서 소장 조회</section>
-    </div>
+      </div>
+      <div className={styles.bookOverview}>
+        <h1 className={styles.bookTitle}>{title}</h1>
+        <ul>
+          {bookSummary.map((summary) => (
+            <li key={summary}>{summary}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.bookDescription}>
+        <h2>책 소개</h2>
+        <div className={styles.bookCategory}>
+          <h3>책 분야</h3>
+          <p>{category}</p>
+        </div>
+        <div className={styles.bookContents}>
+          <h3>책 내용</h3>
+          <pre>{contents}</pre>
+        </div>
+        <div className={styles.bookInfo}>
+          <h3>기본 정보</h3>
+          <table>
+            <caption className="sr-only">{title} 기본 정보</caption>
+            <tbody>
+              <tr>
+                <td>ISBN</td>
+                <td>{isbn}</td>
+              </tr>
+              <tr>
+                <td>출판 일자</td>
+                <td>{formateDate(publicationDate)}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
   );
 };
 
