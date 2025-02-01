c30ed2e33e6b345a94dc1256c377ba090b1294ba
badahertz52 <badahertz52@gmail.com>
2025-02-01 14:44:17 +0900

    test : Searchbar 테스트에 processSearch props 추가

--- src/stories/Searchbar.stories.tsx
+++ src/stories/Searchbar.stories.tsx
@@ -3,6 +3,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Searchbar } from '@/components';
 import { BOOK_SEARCH_CATEGORY_NAME } from '@/constants';
 
+import { ProcessSearchParams } from '../components/search/Searchbar/action/searchAction';
+
 const meta: Meta<typeof Searchbar> = {
   component: Searchbar,
   title: 'components/Search/Searchbar',
@@ -18,6 +20,9 @@ type Story = StoryObj<typeof Searchbar>;
 export const BookSearchbar: Story = {
   args: {
     categoryInfo: BOOK_SEARCH_CATEGORY_NAME,
+    processSearch: ({ selectedCategory, searchValue }: ProcessSearchParams) => {
+      alert(`selectedCategory: ${selectedCategory}, searchValue: ${searchValue}`);
+    },
   },
   render: (args) => (
     <div style={{ width: '30rem' }}>
