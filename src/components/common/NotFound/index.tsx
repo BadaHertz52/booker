e644bb2124d525f04cbed90b5a152cbf0e01ae40
badahertz52 <badahertz52@gmail.com>
2025-02-01 15:50:09 +0900

    feat : not-found 페이지 구현 및 테스트 보강 (#31)

--- src/components/common/NotFound/index.tsx
+++ src/components/common/NotFound/index.tsx
@@ -1,23 +1,24 @@
 import Image from 'next/image';
 import React from 'react';
 
+import { gray200BlurDataURL } from '@/constants';
 import NotFoundImage from '@/images/notFound.svg';
 
 import styles from './index.module.scss';
 
-interface NotFoundSearchResultProps {
+interface NotFoundProps {
   children: React.ReactNode;
 }
 
-const NotFoundSearchResult = ({ children }: NotFoundSearchResultProps) => {
+const NotFound = ({ children }: NotFoundProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.imageWrapper}>
-        <Image alt="" fill src={NotFoundImage} />
+        <Image alt="" fill src={NotFoundImage} placeholder="blur" blurDataURL={gray200BlurDataURL} />
       </div>
       {children}
     </div>
   );
 };
 
-export default NotFoundSearchResult;
+export default NotFound;
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,2 +1,1 @@
 export { default as Searchbar } from './Searchbar';
-export { default as NotFoundSearchResult } from './NotFoundSearchResult';
