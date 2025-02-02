c5cb0e1dd41f9ce5f794da831386aa2031859933
badahertz52 <badahertz52@gmail.com>
2025-02-02 18:22:51 +0900

    refactor : Skeleton,Loadded 분리를 위한 BookInfinityCarousel 합성 컴포넌트로 변경

--- src/stories/BooksInfinityCarousel.stories.tsx
+++ src/stories/BooksInfinityCarousel.stories.tsx
@@ -5,9 +5,9 @@ import { RECOMMENDED_BOOKS_MOCK_DATA } from '@/mocks/mockData';
 
 const meta = {
   title: 'Components/Carousel/BooksInfinityCarousel',
-  component: BooksInfinityCarousel,
+  component: BooksInfinityCarousel.Content,
   tags: ['autodocs'],
-} satisfies Meta<typeof BooksInfinityCarousel>;
+} satisfies Meta<typeof BooksInfinityCarousel.Content>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -17,4 +17,8 @@ export const Default: Story = {
     booksSimpleInfo: RECOMMENDED_BOOKS_MOCK_DATA,
     title: '추천 도서',
   },
+};
+
+export const Skeleton: StoryObj<typeof BooksInfinityCarousel.Skeleton> = {
+  render: () => <BooksInfinityCarousel.Skeleton />,
 };
