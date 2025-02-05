2d06702e69545b41c11f667647d7bcb746049a6a
badahertz52 <badahertz52@gmail.com>
2025-02-05 21:25:30 +0900

    chore : 국립 중앙 도서관 도서 커버 이미지 사용 허가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'www.nl.go.kr',
+      },
+    ],
+  },
 };
-
 export default nextConfig;
