f3d6562b4d3dd1c42e86b3761beb0ffa880ce20d
badahertz52 <badahertz52@gmail.com>
2025-02-06 22:22:12 +0900

    docs : throwRequestError 설명 주석 추가

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -5,6 +5,12 @@ interface ErrorHandlerProps {
   errorMessage: string;
   errorName: string;
 }
+/**
+ * 요청 오류 처리
+ * @param statusCode 상태 코드
+ * @param errorMessage 오류 메시지
+ * @param errorName 오류 이름
+ */
 export const throwRequestError = ({ statusCode, errorMessage, errorName }: ErrorHandlerProps) => {
   const error = new CustomError({
     message: errorMessage,
