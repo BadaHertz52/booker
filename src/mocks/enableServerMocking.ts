712c852a491fdcbc42c3cb2e555b9f91f26d4ca8
badahertz52 <badahertz52@gmail.com>
2025-02-06 23:13:10 +0900

    fix : CI일 때, CI라는 환경 변수를 추가하고, CI에서 목 서버 활성화 시도
    - NODE_ENV를 develop으로 변하는 것은 다른 빌드 환경에 영향을 줌

--- src/mocks/enableServerMocking.ts
+++ src/mocks/enableServerMocking.ts
@@ -1,5 +1,5 @@
 export const enableServerMocking = async () => {
-  if (process.env.NODE_ENV === 'production') return;
+  if (process.env.NODE_ENV === 'production' && !process.env.CI) return;
 
   const { server } = await import('./server');
   server.listen({ onUnhandledRequest: 'bypass' });
