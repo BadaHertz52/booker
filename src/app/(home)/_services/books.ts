c970d68d82364903c0d1df0939f4682ec741582b
badahertz52 <badahertz52@gmail.com>
2025-02-11 12:21:59 +0900

    feat: 홈페이지에 인기 대출 도서 API 적용

---
+++ src/app/(home)/_services/books.ts
@@ -0,0 +1,41 @@
+import { fetchPopularBooks } from '@/services';
+import { BookItemData, PopularBookData } from '@/types';
+
+const formatePopularBookDataToBookItemData = (doc: PopularBookData) => {
+  let author = doc.authors;
+  let translator;
+  if (doc.authors.includes('지은이')) {
+    author = doc.authors
+      .split(';')[0]
+      .trim()
+      .replace(/지은이|:/, '');
+  }
+  if (doc.authors.includes('옮긴이')) {
+    translator = doc.authors
+      .split(';')[1]
+      .trim()
+      .replace(/옮긴이|:/, '');
+  }
+
+  const book: BookItemData = {
+    title: doc.bookname,
+    isbn: doc.isbn13,
+    author,
+    translator,
+    publisher: doc.publisher,
+    coverImageUrl: doc.bookImageURL,
+    publicationYear: Number(doc.publication_year),
+  };
+
+  return book;
+};
+
+export const getPopularBooks = async (): Promise<BookItemData[]> => {
+  const data = await fetchPopularBooks();
+
+  const popularBooks: BookItemData[] = data.docs.map((doc: PopularBookData) => {
+    return formatePopularBookDataToBookItemData(doc.doc);
+  });
+
+  return popularBooks;
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,17 +3,20 @@ import { BOOK_LIST_MOCK_DATA } from '@/mocks/mockData';
 import BookListSection from './_components/BookListSection';
 import BooksCarouselSection from './_components/BooksCarouselSection';
 import { getLastMonthLibrarianPick } from './_services';
+import { getPopularBooks } from './_services/books';
 import styles from './page.module.scss';
 
 const Home = async () => {
   const hotBooks = BOOK_LIST_MOCK_DATA;
   const lastMonthLibrarianPick = await getLastMonthLibrarianPick();
+  const popularBooks = await getPopularBooks();
+  console.log('✨✨✨✨✨', popularBooks);
 
   return (
     <div className={styles.homeContainer}>
       <h1 className="sr-only">BOOKER 홈페이지</h1>
       <BooksCarouselSection.Loaded title="사서 추천 도서" booksSimpleInfo={lastMonthLibrarianPick} />
-      <BookListSection.Loaded title="대출 급상승! 도서" bookItemsData={hotBooks} />
+      <BookListSection.Loaded title="인기 대출 도서" bookItemsData={popularBooks} />
       <BookListSection.Loaded title="다독자를 위한 추천 도서" bookItemsData={hotBooks} />
     </div>
   );
