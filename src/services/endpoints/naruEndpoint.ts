c6235ea3737514cdbbe91acb98df5854c1b128be
badahertz52 <badahertz52@gmail.com>
2025-02-11 14:01:37 +0900

    feat : 대출 급상승 도서 endpoint, 에러 메세지 추가

--- src/services/endpoints/naruEndpoint.ts
+++ src/services/endpoints/naruEndpoint.ts
@@ -1,20 +1,33 @@
-import { getCurrentAndPastWeek } from '@/utils';
+import { getCurrentAndPastWeek, getPastDate } from '@/utils';
 
 const NARU_BASE_URL = 'http://data4library.kr/api';
+const authKey = process.env.NEXT_BOOK_NARU_API_KEY || '';
+const format = 'json';
 
 const getPopularBooksParams = () => {
   const { currentDate: endDt, onWeekago: startDt } = getCurrentAndPastWeek();
 
   return new URLSearchParams({
-    authKey: process.env.NEXT_BOOK_NARU_API_KEY || '',
+    authKey,
     startDt,
     endDt,
     pageNo: '1',
     pageSize: '5',
-    format: 'json',
+    format,
+  });
+};
+/**
+ * 어제일 기준 대출 급상승 도서 목록 API 요청 url 파라미터
+ */
+const getRisingBooksParams = () => {
+  return new URLSearchParams({
+    authKey,
+    searchDt: getPastDate(1),
+    format,
   });
 };
 
 export const naruEndpoint = {
   popularBooks: NARU_BASE_URL + '/loanItemSrch' + '?' + getPopularBooksParams(),
+  risingBooks: NARU_BASE_URL + '/hotTrend' + '?' + getRisingBooksParams(),
 };
