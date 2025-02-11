c5c6c6590e0e3b5f650b25d87cc0c1d5f9168ca6
badahertz52 <badahertz52@gmail.com>
2025-02-11 16:36:10 +0900

    docs : 홈페이지에서 다독자를 위한 추천 도서 삭제

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,7 +7,6 @@ import { getPopularBooks, getRisingBooks } from './_services/books';
 import styles from './page.module.scss';
 
 const Home = async () => {
-  const hotBooks = BOOK_LIST_MOCK_DATA;
   const lastMonthLibrarianPick = await getLastMonthLibrarianPick();
   const popularBooks = await getPopularBooks();
   const risingBooks = await getRisingBooks();
@@ -18,7 +17,6 @@ const Home = async () => {
       <BooksCarouselSection.Loaded title="사서 추천 도서" booksSimpleInfo={lastMonthLibrarianPick} />
       <BookListSection.Loaded title="인기 대출 도서" bookItemsData={popularBooks} />
       <BookListSection.Loaded title="급상승! 대출 도서" bookItemsData={risingBooks} />
-      <BookListSection.Loaded title="다독자를 위한 추천 도서" bookItemsData={hotBooks} />
     </div>
   );
 };
