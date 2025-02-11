677674ecd17cb1a0b5d463f2601c9be8dcd78bca
badahertz52 <badahertz52@gmail.com>
2025-02-11 14:02:35 +0900

    feat : 저자, 옮긴이 포맷팅하는 유틸 함수 분리 및 케이스 추가

--- src/app/(home)/_services/books.ts
+++ src/app/(home)/_services/books.ts
@@ -1,22 +1,40 @@
-import { fetchPopularBooks } from '@/services';
+import { fetchPopularBooks, fetchRisingBooks } from '@/services';
 import { BookItemData, NaruApiBookData } from '@/types';
 
-const formatePopularBookDataToBookItemData = (doc: NaruApiBookData) => {
+const formatAuthors = (doc: NaruApiBookData) => {
   let author = doc.authors;
   let translator;
-
-  if (doc.authors.includes('지은이')) {
+  // case1 : "지은이:~; 옮긴이:~"
+  if (doc.authors.includes('지은이:')) {
     author = doc.authors
       .split(';')[0]
       .trim()
       .replace(/지은이:/, '');
   }
-  if (doc.authors.includes('옮긴이')) {
+  if (doc.authors.includes('옮긴이:')) {
     translator = doc.authors
       .split(';')[1]
       .trim()
       .replace(/옮긴이:/, '');
   }
+  // case2 : "~(지은이), ~(옮긴이)"
+  if (doc.authors.includes('(지은이)')) {
+    author = doc.authors
+      .split(',')[0]
+      .trim()
+      .replace(/(지은이)/, '');
+  }
+  if (doc.authors.includes('(옮긴이)')) {
+    translator = doc.authors
+      .split(',')[0]
+      .trim()
+      .replace(/(옮긴이)/, '');
+  }
+
+  return { author, translator };
+};
+const formatNaruApiBookDataToBookItemData = (doc: NaruApiBookData) => {
+  const { author, translator } = formatAuthors(doc);
 
   const book: BookItemData = {
     title: doc.bookname,
@@ -34,8 +52,11 @@ const formatePopularBookDataToBookItemData = (doc: NaruApiBookData) => {
 export const getPopularBooks = async (): Promise<BookItemData[]> => {
   const data = await fetchPopularBooks();
 
-  const popularBooks: BookItemData[] = data.docs.map((doc: NaruApiBookData) => {
-    return formatePopularBookDataToBookItemData(doc.doc);
+  return data.response.docs.map(({ doc }: { doc: NaruApiBookData }) => {
+    return formatNaruApiBookDataToBookItemData(doc);
+  });
+};
+
   });
 
   return popularBooks;
