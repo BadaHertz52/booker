306279ea1276fdb5bbb0abd2779e32877769940e
badahertz52 <badahertz52@gmail.com>
2025-02-12 00:25:55 +0900

    docs: Observer에서 더 이상 불러올 도서가 없다는 메세지 삭제

--- src/app/search/_components/Observer/index.tsx
+++ src/app/search/_components/Observer/index.tsx
@@ -7,15 +7,7 @@ const Observer = (params: UseFetchBooksActionParams) => {
   const { fetchMoreBooksAction, isPending, isLastPage } = useFetchBooksAction(params);
   const { observerTargetRef } = useObserver({ isLastPage, isPending, fetchMoreBooksAction });
 
-  return (
-    <>
-      {isLastPage && !params.initialIsLastPage ? (
-        <p>더 이상 불러올 도서 목록이 없어요</p>
-      ) : (
-        <div ref={observerTargetRef} style={{ height: '1px' }} />
-      )}
-    </>
-  );
+  return <>{!isLastPage && <div ref={observerTargetRef} style={{ height: '1px' }} />}</>;
 };
 
 export default Observer;
