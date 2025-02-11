330e7db60a8690f0a1daa64c0b87f219a91eed52
badahertz52 <badahertz52@gmail.com>
2025-02-11 17:59:01 +0900

    feat : Suspense를 추가해, 홈 페이지의 도서 목록
    - 서버 컴포넌트인 홈 페이지에서 비동기 서버 컴포넌트인 도서 목록을 Suspense로 감싸서 streaming이 동작되어서 dx 개선되도록 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { BOOK_LIST_MOCK_DATA } from '@/mocks/mockData';
+import { Suspense } from 'react';
 
 import BookListSection from './_components/BookListSection';
 import BooksCarouselSection from './_components/BooksCarouselSection';
@@ -6,17 +6,41 @@ import { getLastMonthLibrarianPick } from './_services';
 import { getPopularBooks, getRisingBooks } from './_services/books';
 import styles from './page.module.scss';
 
-const Home = async () => {
-  const lastMonthLibrarianPick = await getLastMonthLibrarianPick();
-  const popularBooks = await getPopularBooks();
-  const risingBooks = await getRisingBooks();
+const TITLE = {
+  librarianPick: '사서 추천 도서',
+  popular: '인기 대출 도서',
+  risingBooks: '급상승! 대출 도서',
+};
+const SKELTON_LIST_LENGTH = 5;
 
+const AsyncBooksCarousel = async () => {
+  const lastMonthLibrarianPick = await getLastMonthLibrarianPick();
+
+  return <BooksCarouselSection.Loaded title={TITLE.librarianPick} booksSimpleInfo={lastMonthLibrarianPick} />;
+};
+
+interface AsyncBooksParams {
+  title: string;
+  getBooks: () => Promise<any>;
+}
+const AsyncBooks = async ({ title, getBooks }: AsyncBooksParams) => {
+  const books = await getBooks();
+  return <BookListSection.Loaded title={title} bookItemsData={books} />;
+};
+
+const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <h1 className="sr-only">BOOKER 홈페이지</h1>
-      <BooksCarouselSection.Loaded title="사서 추천 도서" booksSimpleInfo={lastMonthLibrarianPick} />
-      <BookListSection.Loaded title="인기 대출 도서" bookItemsData={popularBooks} />
-      <BookListSection.Loaded title="급상승! 대출 도서" bookItemsData={risingBooks} />
+      <Suspense fallback={<BooksCarouselSection.Skeleton title={TITLE.librarianPick} />}>
+        <AsyncBooksCarousel />
+      </Suspense>
+      <Suspense fallback={<BookListSection.Skeleton listTitle={TITLE.popular} listLength={SKELTON_LIST_LENGTH} />}>
+        <AsyncBooks title={TITLE.popular} getBooks={getPopularBooks} />
+      </Suspense>
+      <Suspense fallback={<BookListSection.Skeleton listTitle={TITLE.risingBooks} listLength={SKELTON_LIST_LENGTH} />}>
+        <AsyncBooks title={TITLE.popular} getBooks={getRisingBooks} />
+      </Suspense>
     </div>
   );
 };
