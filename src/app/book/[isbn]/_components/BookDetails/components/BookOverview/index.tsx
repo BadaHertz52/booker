3d56b4ae5945366514cfb11d1708dd7a245e065b
badahertz52 <badahertz52@gmail.com>
2025-02-14 09:37:06 +0900

    fix: 도서 상세 페이지에서 대출 정보 없는 데이터 처리

--- src/app/book/[isbn]/_components/BookDetails/components/BookOverview/index.tsx
+++ src/app/book/[isbn]/_components/BookDetails/components/BookOverview/index.tsx
@@ -11,11 +11,11 @@ interface BookOverviewLoadedProps {
 const BookOverviewLoaded = ({ bookDetailData }: BookOverviewLoadedProps) => {
   const { author, publisher, publicationYear, loans, title } = bookDetailData;
 
-  const bookSummary = [
-    `${author} 저자(글)`,
-    `${publisher}﹒${publicationYear}년`,
-    `대출 ${loans.rank}위 (${loans.count}건)`,
-  ];
+  const bookSummary = [`${author} 저자(글)`, `${publisher}﹒${publicationYear}년`];
+
+  if (loans.count && loans.rank) {
+    bookSummary.push(`대출 ${loans.rank}위 (${loans.count}건)`);
+  }
 
   return (
     <div className={styles.bookOverview}>
