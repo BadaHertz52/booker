5854d3d5a5446a0744e0578b6c3cb046a6f65d84
badahertz52 <badahertz52@gmail.com>
2025-02-20 18:58:05 +0900

    feat: 도서 목록 페이지 - 스크린 리더기에 도서 목록 안내 제공
    - 도서 목록 페이지에서 스크린 리더기 사용자에게 총 도서 목록와 현재 도서 목록, 스크롤 가능 여부 안내

--- src/app/books/[list]/page.tsx
+++ src/app/books/[list]/page.tsx
@@ -5,9 +5,20 @@ import { BooksPageParams } from './layout';
 const BooksPage = async ({ params }: { params: BooksPageParams }) => {
   const { list } = await params;
 
-  const { newBooks: initialBooks, lastPage: initialIsLastPage } = await loadNewBooks({ list, nextPage: 1 });
+  const {
+    newBooks: initialBooks,
+    lastPage: initialIsLastPage,
+    totalBooksLength,
+  } = await loadNewBooks({ list, nextPage: 1 });
 
-  return <Contents list={list} initialBooks={initialBooks} initialIsLastPage={initialIsLastPage} />;
+  return (
+    <Contents
+      list={list}
+      initialBooks={initialBooks}
+      initialIsLastPage={initialIsLastPage}
+      totalBooksLength={totalBooksLength}
+    />
+  );
 };
 
 export default BooksPage;
